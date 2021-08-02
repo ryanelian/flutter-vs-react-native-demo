177e295bdbd8b166749f8371aecb4fc04373d7b4
Ryan Elian <ryan.elian.cs@gmail.com>
2021-08-02 17:23:31 +0700

    Add refresh animation on flutter demo

--- demo/lib/DetailsPage.dart
+++ demo/lib/DetailsPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class DetailsPage extends StatefulWidget {
   @override
@@ -9,6 +10,18 @@ class DetailsPage extends StatefulWidget {
 }
 
 class DetailsPageState extends State<DetailsPage> {
+  List<String> items = ["1", "2", "3", "4", "5", "6", "7", "8"];
+
+  RefreshController refreshController =
+      RefreshController(initialRefresh: false);
+
+  void onRefresh() async {
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    refreshController.refreshCompleted();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,8 +29,17 @@ class DetailsPageState extends State<DetailsPage> {
         title: Text('Details'),
       ),
       body: Container(
-        child: Text('Hello World!')
-      ),
+          child: SmartRefresher(
+        enablePullDown: true,
+        controller: refreshController,
+        header: ClassicHeader(),
+        onRefresh: onRefresh,
+        child: ListView.builder(
+          itemBuilder: (c, i) => Card(child: Center(child: Text(items[i]))),
+          itemExtent: 100.0,
+          itemCount: items.length,
+        ),
+      )),
     );
   }
 }
